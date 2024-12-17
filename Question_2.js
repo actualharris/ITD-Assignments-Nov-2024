0610cc85c6f9131dad67ffbb78a32f690daab8f5
actualharris <github.actualharris@slalias.nl>
2024-12-17 22:03:47 +0530

    update q2

--- Question_2.js
+++ Question_2.js
@@ -1,3 +1,6 @@
+// Write logic to check if the user is 18 years or older than 18 years allow him to vote, 
+// else infrom him how many more years he needs to become a voter.
+
 let age = parseInt(prompt("Please enter your age:"));
 
 if (age >= 18) {
